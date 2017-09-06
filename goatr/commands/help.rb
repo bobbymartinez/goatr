108082989ec73e7451d4f28ee6ea715331b3342e
Bobby Martinez <amusicalmexican@gmail.com>
2017-09-06 17:50:11 -0500

    fixed formatting on help output

--- goatr/commands/help.rb
+++ goatr/commands/help.rb
@@ -4,8 +4,8 @@ module Goatr
       command 'help' do |client, data, _match|
         client.say(channel: data.channel,
         text: %{ Are you trying to start an incident?  Try "goatr start incident <incident name less than 22 characters>"
-          That should get you where you need to be.
-          See https://github.com/bobbymartinez/goatr for more command information on commands.'})
+That should get you where you need to be.
+See https://github.com/bobbymartinez/goatr for more command information on commands.})
       end
     end
   end
